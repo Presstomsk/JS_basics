7c87e1f861b68581dca7d2c21b210bee5e9aad95
Roman Permiakov <info@presstomsk.ru>
2022-07-19 11:42:53 +0700

    2
    Запросите у пользователя 2 числа и выведите среднее арифметическое этих чисел.

---
+++ 2/script.js
@@ -0,0 +1,3 @@
+let firstNumber = Number(prompt("Введите число"));
+let secondNumber = Number(prompt("Введите число"));
+alert(`среднее арифметическое чисел ${firstNumber} и ${secondNumber} равно ${(firstNumber+secondNumber)/2}`);
